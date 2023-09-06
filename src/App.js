0b7d3508e6ea4e412493c55aa111daa485390312
chantel-mills <chantel.mills@student.uts.edu.au>
2023-09-06 16:01:27 +1000

    remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 
 import subjectImage from './white_paper.jpg'
@@ -19,34 +18,6 @@ function SubjectCard() {
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <h1>HELLOOOOOO WORLD!</h1>
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-
-    //     {/* <br></br> */}
-
-    //     {/* <button>
-    //       <img src="./wallpaper-one.jpg" alt="test button" />
-    //       <label>Test Button</label>
-    //     </button> */}
-
-    //     {/* <div style={{ backgroundImage: testImage }}>Overlay text</div> */}
-
-    //   </header>
-    // </div>
-
     <div className="dashboard-layout">
       <div className="dashboard-header">
         <div className="dashboard-row">
@@ -54,16 +25,9 @@ function App() {
             <p className="white-text">Hi, Account Name</p>
           </div>
           <div className="column-padding-4-percent padding-header-button">
-            <button className="nice-button">Sign out</button>
+            <button className="header-button">Sign out</button>
           </div>
-          {/* <div className="column-padding-20-percent">
-            <p className="white-circle white-text">circ</p>
-          </div> */}
-          {/* <div className="column-padding-25-percent">
-            <p className="white-box white-text">Teacher Dashboard</p>
-          </div> */}
         </div>
-        {/* <h1>HELLOOOOO WOrLd!</h1> */}
       </div>
 
       <div className="dashboard-main-content">
