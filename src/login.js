c41e8a21d3a6cfdc712b9edb4d0d543d8adf27ee
Pet3rP <piradon291@gmail.com>
2023-09-15 17:13:19 +1000

    success

--- src/login.js
+++ src/login.js
@@ -4,6 +4,7 @@ import './login.css';
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loginStatus, setLoginStatus] = useState('');
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,14 +16,21 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
+    
+    // Check if the entered email and password match any user in the list
+    const user = users.find((user) => user.email === email && user.password === password);
+
+    if (user) {
+      setLoginStatus('Login successful');
+      // You can perform further actions here, such as redirecting to another page
+    } else {
+      setLoginStatus('Login failed');
+      // Display an error message or handle the login failure as needed
+    }
   };
-  
 
   return (
     <div className="login-container light-blue-background">
-      
       <div className="login-form">
         <h2>Login</h2>
         <form onSubmit={handleSubmit}>
@@ -36,13 +44,17 @@ function Login() {
           </div>
           <button type="submit" className="login-button">Login</button>
         </form>
+        {/* Display login status */}
+        {loginStatus && <p className={`login-status ${loginStatus === 'Login successful' ? 'success' : 'failure'}`}>{loginStatus}</p>}
       </div>
     </div>
   );
 }
 
+const users = [
+  { email: 'user1@example.com', password: 'password1' },
+  { email: 'user2@example.com', password: 'password2' },
+  // Add more users as needed
+];
+
 export default Login;
-
-
-
-
