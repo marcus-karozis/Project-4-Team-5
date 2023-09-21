5543d3efb7db9f0e3771ae827211c7c8b77d5006
unknown <felicialo888@gmail.com>
2023-09-22 00:27:57 +1000

    facial recognition

--- src/login.js
+++ src/login.js
@@ -1,63 +1,73 @@
 import React, { useState } from 'react';
-import './login.css';
+import { FaceClient } from '@azure/cognitiveservices-face';
 
+// Initialize the Azure Face API client
+const faceClient = new FaceClient('<Your-API-KEY>', '<Your-API-ENDPOINT>');
 
-
-
-function LoginPage() {
+const AuthenticationPage = () => {
+  const [showLoginForm, setShowLoginForm] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [canSubmit, setCanSubmit] = useState(false);
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-    checkCanSubmit(e.target.value, password);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    checkCanSubmit(username, e.target.value);
-  };
-
-  const checkCanSubmit = (uname, pword) => {
-    if (uname && pword) {
-      setCanSubmit(true);
-    } else {
-      setCanSubmit(false);
+  const handleFaceRecognition = async () => {
+    // Assume faceImageData contains the face image data
+    const faceImageData = ''; // Fetch this data from your UI
+    try {
+      const result = await faceClient.face.detectWithStream(faceImageData);
+      if (result.length > 0) {
+        // Face detected successfully, proceed with your logic
+      } else {
+        // No face detected or some error occurred
+        setShowLoginForm(true);
+      }
+    } catch (error) {
+      console.error("Azure Face API Error:", error);
+      setShowLoginForm(true);
     }
   };
 
   const handleLogin = () => {
-    // Here you would make the API call to your backend to validate the login
-    console.log('Logging in with', username, password);
+    // Your login logic here
   };
 
   return (
-    <div className="login-container light-blue-background">
-  <div className="login-form">
-    <h2>Login</h2>
-    <form onSubmit={handleLogin}>
-      <div className="form-group">
-        <label>Email:</label>
-        <input type="email" value={username} onChange={handleUsernameChange} required />
-      </div>
-      <div className="form-group">
-        <label>Password:</label>
-        <input type="password" value={password} onChange={handlePasswordChange} required />
-      </div>
-      <button type="submit" className="login-button">Login</button>
-    </form>
-    <div className="forgot-password-container">
-      <a href="/forgot-password">Forgot Password?</a>
+    <div className="authentication-container">
+      {!showLoginForm ? (
+        <>
+          <h2>Facial Recognition</h2>
+          {/* Your Facial Recognition UI here */}
+          <button onClick={handleFaceRecognition}>Verify Face</button>
+        </>
+      ) : (
+        <>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </form>
+        </>
+      )}
     </div>
-  </div>
-</div>
-
   );
-}
+};
 
-export default LoginPage;
-
-
-
-
+export default AuthenticationPage;
