c5f5dad9db97b372ea55b2813768cd4dd4910e7d
chantel-mills <chantel.mills@student.uts.edu.au>
2023-09-27 13:44:40 +1000

    send email to "admin" from tech support page

--- src/techSupportPage/TechSupportPage.js
+++ src/techSupportPage/TechSupportPage.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './TechSupportPageStyles.css';
 import Navbar from '../components/Navbar';
+import emailjs from '@emailjs/browser';
 
 function TechSupportPage() {
+
+    // const fullNameInputField = useRef();
+    // const emailInputField = useRef();
+    // const issueInputField = useRef();
+
+    const formThing = useRef();
 
     function handleSubmitSupportForm(e) {
         // Prevent the browser from reloading the page
@@ -10,14 +17,37 @@ function TechSupportPage() {
 
         // Read the form data
         const form = e.target;
+
+        // console.log("------");
+        // console.log(form);
+        // console.log(form.current);
+
         const formData = new FormData(form);
+
+        // console.log(formData);
+        // console.log(formData.current);
 
         // obtain all the data on the form & store in object
         const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
+        // console.log(formJson);
 
-        // SEND EMAIL TO ADMIN USER
-        // TODO!
+        // console.log("..");
+        // console.log(formThings.current)
+
+        // // send email to admin user
+        // // note: I have this commented out because there's a limit to how many emails 
+        // // can be sent per month
+        // // also the details to access where the email is sent will be adding the details into the 
+        // // jira ticket for the tech support page :) -C
+
+        // // comment below back in if you want to send a test email for support page
+        // emailjs.sendForm('service_gs0av92', 'template_2y5bxxs', formThing.current, 'KNHae1w2H-IMWhEI7')
+        //     .then((result) => {
+        //         console.log(result.text);
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
+        // // comment above back in if you want to send a test email for support page
 
         var responseBox = document.getElementById("tech_support_form_response");
         responseBox.style.display='block';
@@ -31,13 +61,13 @@ function TechSupportPage() {
 
             <div className="page_padding" style={{textAlign: 'center'}}>
                 <h1>Tech Support Page</h1>
-                <form method="post" onSubmit={handleSubmitSupportForm}>
+                <form method="post" ref={formThing} onSubmit={handleSubmitSupportForm}>
                     <div id="name_box">
                         {/* <div className="label_div" style={{}}>
                             <label > Name </label>
                         </div> */}
                         <div>
-                            <input name="nameInput" className="" required placeholder='Name' />
+                            <input name="name_input" className="" required placeholder='Name' />
                         </div>
                     </div>
 
@@ -46,7 +76,7 @@ function TechSupportPage() {
                             <label > Email </label>
                         </div> */}
                         <div>
-                            <input name="emailInput" className="" required placeholder='Email' />
+                            <input type="email" name="email_input" className="" required placeholder='Email' />
                         </div>
                     </div>
 
@@ -55,7 +85,7 @@ function TechSupportPage() {
                             <label > Issue </label>
                         </div> */}
                         <div>
-                            <textarea name="issueInput" rows={8} cols={40} required placeholder='Issue/Enquiry' />
+                            <textarea name="issue_input" rows={8} cols={40} required placeholder='Issue/Enquiry' />
                         </div>
                     </div>
 
