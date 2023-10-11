405b5516ccc209c2e00e4d2f72f78d0d2dc5434c
Piradon Phuangthong <129824695+Pet3rP@users.noreply.github.com>
2023-10-11 12:14:10 +1100

    Update login.js, name change, files from login is in faceA

--- src/login.js
+++ src/login.js
@@ -1,72 +1,63 @@
-import React, { useRef, useState, useEffect } from 'react';
-import Webcam from 'react-webcam';
+import React, { useState } from 'react';
+import './login.css';
 
-const AuthenticationPage = () => {
-  const webcamRef = useRef(null);
-  const [authStatus, setAuthStatus] = useState(null);
-  const [username, setUsername] = useState(null);
 
-  const handleFaceRecognition = async () => {
-    const imageSrc = webcamRef.current.getScreenshot();
 
-    try {
-      const formData = new FormData();
-      formData.append('image', imageSrc);
 
-      const response = await fetch('http://localhost:5000/login_with_face', {
-        method: 'POST',
-        body: formData
-      });
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [canSubmit, setCanSubmit] = useState(false);
 
-      const result = await response.json();
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+    checkCanSubmit(e.target.value, password);
+  };
 
-      if (result.status === "success") {
-        setAuthStatus("success");
-        setUsername(result.username);
-      } else {
-        setAuthStatus("fail");
-        setUsername(null);
-      }
-    } catch (error) {
-      setAuthStatus("error");
-      setUsername(null);
-      console.error("Error during face recognition:", error);
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    checkCanSubmit(username, e.target.value);
+  };
+
+  const checkCanSubmit = (uname, pword) => {
+    if (uname && pword) {
+      setCanSubmit(true);
+    } else {
+      setCanSubmit(false);
     }
   };
 
-  useEffect(() => {
-    // Setting a delay to ensure the webcam is ready
-    const timeoutId = setTimeout(handleFaceRecognition, 3000);
-    return () => clearTimeout(timeoutId); // Cleanup to avoid memory leaks
-  }, []);
+  const handleLogin = () => {
+    // Here you would make the API call to your backend to validate the login
+    console.log('Logging in with', username, password);
+  };
 
   return (
-    <div className="authentication-container">
-      <h2>Facial Recognition</h2>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-      />
-      {authStatus === "success" ? (
-        <p>Login Successful as {username}</p>
-      ) : (
-        <p>Authenticating...</p>
-      )}
-      {authStatus === "fail" && (
-        <div>
-          <p>Face not recognized or authentication failed.</p>
-          <button onClick={handleFaceRecognition}>Try Again</button>
-        </div>
-      )}
-      {authStatus === "error" && (
-        <div>
-          <p>An error occurred. Please try again.</p>
-          <button onClick={handleFaceRecognition}>Try Again</button>
-        </div>
-      )}
+    <div className="login-container light-blue-background">
+  <div className="login-form">
+    <h2>Login</h2>
+    <form onSubmit={handleLogin}>
+      <div className="form-group">
+        <label>Email:</label>
+        <input type="email" value={username} onChange={handleUsernameChange} required />
+      </div>
+      <div className="form-group">
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} required />
+      </div>
+      <button type="submit" className="login-button">Login</button>
+    </form>
+    <div className="forgot-password-container">
+      <a href="/forgot-password">Forgot Password?</a>
     </div>
-  );
-};
+  </div>
+</div>
 
-export default AuthenticationPage;
+  );
+}
+
+export default LoginPage;
+
+
+
+
