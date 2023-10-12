356af2ca3f6115fcd47767fa2d83e073eaf3edce
Piradon Phuangthong <129824695+Pet3rP@users.noreply.github.com>
2023-10-12 03:17:11 +0000

    Update login.js, demo

--- src/login.js
+++ src/login.js
@@ -1,72 +1,107 @@
 import React, { useRef, useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
 
-const AuthenticationPage = () => {
+const AuthenticationPage = ({ onFail }) => {
   const webcamRef = useRef(null);
   const [authStatus, setAuthStatus] = useState(null);
-  const [username, setUsername] = useState(null);
-
-  const handleFaceRecognition = async () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-
-    try {
-      const formData = new FormData();
-      formData.append('image', imageSrc);
-
-      const response = await fetch('http://localhost:5000/login_with_face', {
-        method: 'POST',
-        body: formData
-      });
-
-      const result = await response.json();
-
-      if (result.status === "success") {
-        setAuthStatus("success");
-        setUsername(result.username);
-      } else {
-        setAuthStatus("fail");
-        setUsername(null);
-      }
-    } catch (error) {
-      setAuthStatus("error");
-      setUsername(null);
-      console.error("Error during face recognition:", error);
-    }
-  };
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
 
   useEffect(() => {
-    // Setting a delay to ensure the webcam is ready
-    const timeoutId = setTimeout(handleFaceRecognition, 3000);
-    return () => clearTimeout(timeoutId); // Cleanup to avoid memory leaks
+    const handleKeyPress = (e) => {
+      if (e.key === 'y') {
+        setAuthStatus("success");
+        setEnteredUsername("Max");
+      } else if (e.key === 'n') {
+        setAuthStatus("fail");
+        setTimeout(() => {
+          setAuthStatus("showLoginForm");
+        }, 2000); // wait for 2 seconds before showing the login form
+      }
+    };
+
+    // Add the event listener
+    window.addEventListener('keydown', handleKeyPress);
+
+    // Cleanup the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
   }, []);
 
   return (
-    <div className="authentication-container">
-      <h2>Facial Recognition</h2>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-      />
-      {authStatus === "success" ? (
-        <p>Login Successful as {username}</p>
+    <div className="authentication-container" style={containerStyle}>
+      <h2>Login Page</h2>
+      {authStatus === "showLoginForm" ? (
+        <div style={loginFormStyle}>
+          <div className="input-group" style={inputGroupStyle}>
+            <label>Username:</label>
+            <input 
+              type="text" 
+              placeholder="Enter username" 
+              value={enteredUsername} 
+              onChange={e => setEnteredUsername(e.target.value)} 
+            />
+          </div>
+          <div className="input-group" style={inputGroupStyle}>
+            <label>Password:</label>
+            <input 
+              type="password" 
+              placeholder="Enter password" 
+              value={enteredPassword} 
+              onChange={e => setEnteredPassword(e.target.value)} 
+            />
+          </div>
+          <button onClick={() => {}}>Login</button>
+        </div>
       ) : (
-        <p>Authenticating...</p>
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          style={webcamStyle}
+        />
       )}
-      {authStatus === "fail" && (
-        <div>
-          <p>Face not recognized or authentication failed.</p>
-          <button onClick={handleFaceRecognition}>Try Again</button>
-        </div>
-      )}
-      {authStatus === "error" && (
-        <div>
-          <p>An error occurred. Please try again.</p>
-          <button onClick={handleFaceRecognition}>Try Again</button>
-        </div>
-      )}
+      {authStatus === "success" && <p style={messageStyle}>Login Successful as {enteredUsername}</p>}
+      {authStatus === "fail" && <p style={messageStyle}>Authentication Unsuccessful</p>}
     </div>
   );
+};
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh', // Fill the viewport vertically
+  textAlign: 'center',
+  backgroundColor: 'lightblue' // Setting background to light blue
+};
+
+const loginFormStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '10px', // Decreased spacing for a tighter look
+  width: '300px'
+};
+
+const inputGroupStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
+  marginBottom: '5px' // Reduced space between the username and password
+};
+
+const webcamStyle = {
+  boxShadow: '0px 0px 10px 3px rgba(0,0,0,0.2)' // Adding a soft shadow for some depth
+};
+
+const messageStyle = {
+  marginTop: '20px',
+  fontSize: '18px',
+  fontWeight: 'bold'
 };
 
 export default AuthenticationPage;
