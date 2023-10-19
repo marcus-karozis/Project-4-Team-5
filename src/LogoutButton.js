4bb085c2c19f3da4d5141163302ab98a7fe792c6
unknown <felicialo888@gmail.com>
2023-10-19 13:26:53 +1100

    Logout Button

---
+++ src/LogoutButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './LogoutButton.css';
+
+const LogoutButton = ({ onLogout }) => {
+
+  return (
+
+    <button className="logout-button" onClick={onLogout}>
+
+      Logout
+
+    </button>
+
+  );
+
+};
+
+ 
+
+export default LogoutButton;
