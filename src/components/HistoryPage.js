60ccc1972dde0981924b27937d99d8f3f491007d
oliviakathrynp <olivia.pistolese@student.uts.edu.au>
2023-10-19 21:27:07 +1100

    added history page route

---
+++ src/components/HistoryPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function HistoryPage() {
+
+    return (
+        <>
+            <Navbar></Navbar>
+            <div>History Page </div>
+        </>
+
+    )
+};
+
+export default HistoryPage; 
