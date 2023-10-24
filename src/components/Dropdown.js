5ef7a5c6db3f1f833f7183628644c77a81e3bc20
oliviakathrynp <olivia.pistolese@student.uts.edu.au>
2023-10-24 22:35:04 +1100

    effect on dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,11 +3,15 @@ import "./Dropdown.css";
 const Dropdown = ({ dropdown }) => {
     return (
         <ul className={`dropdown ${dropdown ? "show" : ""}`}>
+             <li className="menu-items">
+                <a href='#'>Personal details</a>
+            </li>
             <li className="menu-items">
                 <a href='#'>Settings</a>
             </li>
+            <hr/>
             <li className="menu-items">
-                <a href='#' onClick="{() => }">Logout</a>
+                <a href='#' onClick="{() => }">Log out</a>
             </li>
         </ul>
     );
