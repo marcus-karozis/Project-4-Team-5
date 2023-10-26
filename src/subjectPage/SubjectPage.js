5eff0994b5b2c50b4efc23c1ead3db775104f945
Kenwyn Chin <kenwynchin@yahoo.com>
2023-10-27 10:27:49 +1100

    sort date

--- src/subjectPage/SubjectPage.js
+++ src/subjectPage/SubjectPage.js
@@ -6,36 +6,37 @@ import { Class } from '../menu/Menu';
 import axios from 'axios'; 
 import { useLocation } from 'react-router-dom'
 
+function getNextClass (arr) {
+    let now = new Date();
+    for (let time of arr){
+        let difference = now.getTime() - new Date(time).getTime();
+        if (difference < 0) {
+            return time;
+        }
+    }
+}
+
+function sortfn (a,b) {
+    let now = new Date();
+    let time1, time2;
+    time1 = getNextClass(a.class_start_timestamps);
+    time2 = getNextClass(b.class_start_timestamps);
+    return time2 - time1;
+}
+
 function SubjectPage() {
     
     let [classData, setClasses] = useState([]);
     let location = useLocation();
     let { subject_id } = location.state;
     useEffect(() => {
-        const sortfn = (a,b) => {
-            let now = new Date();
-            let time1, time2;
-            for (let time of a.class_start_timestamps){
-                let difference = now.getTime() - new Date(time).getTime();
-                if (difference < 0) {
-                    time1 = difference;
-                }
-            }
-            for (let time of b.class_start_timestamps){
-                let difference = now.getTime() - new Date(time).getTime();
-                if (difference < 0) {
-                    time2 = difference;
-                }
-            }
-            return time2 - time1;
-        }
 
         const fetchClass = async () => {
             try {
                 let response = await axios.get('/db/subjects');
                 let classes = response.data.find(s => s._id === subject_id).classes;
-                // classes.sort(sortfn());
-                sortfn(classes, classes);
+                classes.sort(sortfn());
+                // sortfn(classes, classes);
                 setClasses(classes);
             } catch (error) {
                 console.error(error);
@@ -57,7 +58,7 @@ function SubjectPage() {
                         className={_class.class_name}
                         subject_id={subject_id}
                         class_id={classData.indexOf(_class)}
-                        time={new Date(_class.class_start_timestamps[0]).toDateString() ?? ""}
+                        time={new Date(getNextClass(_class.class_start_timestamps)).toDateString() ?? ""}
                     />))
                 }
             </div>
