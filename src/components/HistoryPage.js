fddb71d0cb600c9e9d0e25f9a30b8817e4c1ef77
oliviakathrynp <olivia.pistolese@student.uts.edu.au>
2023-11-02 17:39:36 +1100

    adding history page

--- src/components/HistoryPage.js
+++ src/components/HistoryPage.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
+
+import UserContext from '../usercontext';
 
 function HistoryPage() {
+
+    const { user } = useContext(UserContext);
+    const [ticketData, setTickets] = useState([])
+
+    useEffect(() => {
+        const fetchUserCheckIns = async () => {
+            try {
+                const response = await axios.get('/db/getSubjectsByUserId', {params: {id: user._id}});
+                const tickets = response.data;
+                console.log(JSON.stringify(tickets, null, 2))
+                setTickets(tickets);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUserCheckIns();
+    }, []);
 
     return (
         <>
